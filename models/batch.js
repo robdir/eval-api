29a888045fa68737cc10f7d01d93b30d0661ca2a
robdir <rhwickham@gmail.com>
2017-11-15 12:18:58 +0100

    schema modified

--- models/batch.js
+++ models/batch.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose
 const ratingsSchema = new Schema({
   colour: { type: Number, min: 0, max: 2 }, // 0 is red, 1 orange and 2 green
   comments: { type: String },
-  ratedAt: { type: String }
+  ratedAt: { type: Date, default: Date.now }
 });
 
 const studentSchema = new Schema({
