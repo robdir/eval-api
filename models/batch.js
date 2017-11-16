616bf6f17ddd02b6f53cfd6cb9a00d3b8e8336f8
robdir <rhwickham@gmail.com>
2017-11-16 15:50:09 +0100

    batches

--- models/batch.js
+++ models/batch.js
@@ -2,23 +2,22 @@ const mongoose = require('../config/database')
 const { Schema } = mongoose
 
 const ratingsSchema = new Schema({
-  colour: { type: Number, min: 0, max: 2 }, // 0 is red, 1 orange and 2 green
+  colour: { type: Number, min: 0, max: 2 },
   comments: { type: String },
-  ratedAt: { type: Date, default: Date.now }
+  ratedAt: { type: Date, default: Date.now },
 });
 
 const studentSchema = new Schema({
-  batchId: { type: Schema.Types.ObjectId, ref: 'batches' },
   name: { type: String },
   picture: { type: String },
-  ratings: [{ratingsSchema}]
+  ratings: [ratingsSchema],
 });
 
 const batchSchema = new Schema({
   batchNum: { type: Number },
   startsAt: { type: Date },
   endsAt: { type: Date },
-  students: [{studentSchema}],
+  students: [studentSchema],
 });
 
 
