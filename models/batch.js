ed6c33e44378aa89dd5af06821d9ca35e0ebbaf1
robdir <rhwickham@gmail.com>
2018-01-17 13:14:32 +0100

    working?

--- models/batch.js
+++ models/batch.js
@@ -3,6 +3,19 @@ const { Schema } = mongoose
 const studentschema = require('./student').schema
 
 
+const ratingsSchema = new Schema({
+  colour: { type: Number, min: 0, max: 2 },
+  comments: { type: String },
+  ratedAt: { type: Date, default: Date.now },
+});
+
+const studentSchema = new Schema({
+  batchId: { type: Schema.Types.ObjectId, ref: 'batches', required: true },
+  name: { type: String },
+  picture: { type: String },
+  ratings: [ratingsSchema],
+});
+
 const batchSchema = new Schema({
   batchNum: { type: Number },
   startsAt: { type: Date },
--- models/student.js
+++ models/student.js
@@ -1,17 +1,5 @@
-const mongoose = require('../config/database')
-const {Schema} = mongoose
+// const mongoose = require('../config/database')
+// const {Schema} = mongoose
 
-const ratingsSchema = new Schema({
-    colour: { type: Number, min: 0, max: 2 },
-    comments: { type: String },
-    ratedAt: { type: Date, default: Date.now },
-});
 
-const studentSchema = new Schema({
-    batchId: { type: Schema.Types.ObjectId, ref: 'batches' },
-    name: { type: String },
-    picture: { type: String },
-    ratings: [ratingsSchema],
-});
-
-module.exports = mongoose.model('students', studentSchema)
+// module.exports = mongoose.model('students', studentSchema)
