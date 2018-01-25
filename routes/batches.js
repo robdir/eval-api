3feda8e500843e9c2c813d13146ec9f0fdd006c2
robdir <rhwickham@gmail.com>
2018-01-25 14:02:03 +0100

    routes for batches modified - delete

--- routes/batches.js
+++ routes/batches.js
@@ -31,14 +31,16 @@ router.post('/batches', (req, res, next) => {
     .catch((error) => next(error))
 })
 
-// use findbyIdandUpdate? check express/mongo docs
+// use findbyIdandUpdate? check express/mongo docs ??
 
-// router.patch('./id', findBatch, (req, res, next) => {
-//   let student = req.body
-//   Batch.append(student)
-//     .then((batch) => res.json(batch))
-//     .catch((error) => next(error))
-// })
+router.patch('./:id', (req, res, next) => {
+  const id = req.params.id
+  Batch.findByIdAndUpdate(id)
+  let student = req.body
+  Batch.append(student)
+    .then((batch) => res.json(batch))
+    .catch((error) => next(error))
+})
 
 
 router.delete('/:id', (res, req, next) =>{
